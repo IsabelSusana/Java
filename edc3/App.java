46cec57f0ba89c390e1e7a21ecf714f554a6f0f4
Isabel Susana Suárez <136186826+IsabelSusana@users.noreply.github.com>
2023-11-09 13:29:26 -0600

    Add files via upload

---
+++ edc3/App.java
@@ -0,0 +1,7 @@
+public class App{
+    public static void main(String[] args) throws Exception{
+        int[] numeros={1,2,3,4,5,6,7,8,9,10};
+        int[] numeros2=new int [10];
+        
+    }
+}
---
+++ edc3/Evaluacion.java/Evaluacion.java
@@ -0,0 +1,9 @@
+/*Evaluación comenzamos a escribir la clase*/
+public class Evaluacion{
+    //Define el método main() dentro de las llaves de la clase (class) Evaluacion.//
+    public static void main(String[] args) {
+        //El método main ejecuta las tareas de la clase.
+        System.out.println("¡Mi primer programa Java desde cero!");
+    }
+    //Finalmente el System.out.printIn se encarag de que imprime nuestra aplicación 
+}
